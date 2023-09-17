fe4fa7349b5650e3a65493305f0d8be666ce5b0f
ArtemBi <avatarus2010@yandex.ru>
2023-09-18 02:56:52 +0300

    quest

--- src/test/java/ru/practicum/shareit/UserTest/dto/UserDtoTest.java
+++ src/test/java/ru/practicum/shareit/UserTest/dto/UserDtoTest.java
@@ -64,7 +64,7 @@ public class UserDtoTest {
         userDto.setEmail(" ");
         Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
         assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("должно иметь формат адреса электронной почты");
+        assertThat(violations.toString()).contains("must be a well-formed email address");
     }
 
     @Test
@@ -72,6 +72,6 @@ public class UserDtoTest {
         userDto.setEmail(null);
         Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
         assertThat(violations).isNotEmpty();
-        assertThat(violations.toString()).contains("не должно равняться null");
+        assertThat(violations.toString()).contains("must not be null");
     }
 }
